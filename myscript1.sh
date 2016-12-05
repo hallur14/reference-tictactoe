c1c9f33edb1c1cadf22882a4574ce492ed4160b4
Hallur <hallur14@ru.is>
2016-12-05 14:15:13 +0000

    files for docker

---
+++ myscript1.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo Cleaning...
+rm -rf ./dist
+
+echo "1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+if [ -z "$GIT_COMMIT" ]; then
+  export GIT_COMMIT=$(git rev-parse HEAD)
+  export GIT_URL=$(git config --get remote.origin.url)
+fi
+
+# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
+export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
+
+echo "2!!!!!!!!!!!!!!!!!!!!!!!!1"
+echo Building app
+npm run build
+
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Npm build failed with exit code " $rc
+    exit $rc
+fi
+
+
+cat > ./dist/githash.txt <<_EOF_
+$GIT_COMMIT
+_EOF_
+
+cat > ./dist/public/version.html << _EOF_
+<!doctype html>
+<head>
+   <title>App version information</title>
+</head>
+<body>
+   <span>Origin:</span> <span>$GITHUB_URL</span>
+   <span>Revision:</span> <span>$GIT_COMMIT</span>
+   <p>
+   <div><a href="$GITHUB_URL/commits/$GIT_COMMIT">History of current version</a></div>
+</body>
+_EOF_
+
+
+cp ./Dockerfile ./build/
+
+cd dist
+echo Building docker image
+
+sudo docker build -t hallur14/tictactoe:$GIT_COMMIT .
+
+echo "3!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Docker build failed " $rc
+    exit $rc
+fi
+
+sudo docker push hallur14/tictactoe:$GIT_COMMIT
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Docker push failed " $rc
+    exit $rc
+fi
+
+echo "Done
